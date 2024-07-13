d6e29b7a06dbca0695ea0edf2d2122fbfd5c236c
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2024-07-13 02:53:13 +0200

    draw squares

--- src/renderCanvasWithShaders.js
+++ src/renderCanvasWithShaders.js
@@ -14,6 +14,7 @@ function render(vertexShaderSource, fragmentShaderSource) {
     var program = createProgram(gl, vertexShader, fragmentShader);
     var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
     var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
+    var colorLocation = gl.getUniformLocation(program, "u_color");
     var positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
@@ -48,10 +49,28 @@ function render(vertexShaderSource, fragmentShaderSource) {
     gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
     // Bind the attribute/buffer set we want.
     gl.bindVertexArray(vao);
-    var primitiveType = gl.TRIANGLES;
-    var offset = 0;
-    var count = 6;
-    gl.drawArrays(primitiveType, offset, count);
+    //var primitiveType = gl.TRIANGLES;
+    //var offset = 0;
+    //var count = 6;
+    //gl.drawArrays(primitiveType, offset, count);
+
+    // draw 50 random rectangles in random colors
+    for (var ii = 0; ii < 50; ++ii) {
+      // Setup a random rectangle
+      setRectangle(
+          gl, randomInt(300), randomInt(300), randomInt(300), randomInt(300));
+ 
+      // Set a random color.
+      gl.uniform4f(colorLocation, Math.random(), Math.random(), Math.random(), 1);
+ 
+      // Draw the rectangle.
+      var primitiveType = gl.TRIANGLES;
+      var offset = 0;
+      var count = 6;
+      gl.drawArrays(primitiveType, offset, count);
+    }
+
+
 }
 
 function createShader(gl, type, source) {
@@ -99,6 +118,34 @@ function resizeCanvasToDisplaySize(canvas) {
     }
    
     return needResize;
+  }
+
+
+// Returns a random integer from 0 to range - 1.
+function randomInt(range) {
+    return Math.floor(Math.random() * range);
+  }
+   
+  // Fills the buffer with the values that define a rectangle.
+   
+  function setRectangle(gl, x, y, width, height) {
+    var x1 = x;
+    var x2 = x + width;
+    var y1 = y;
+    var y2 = y + height;
+   
+    // NOTE: gl.bufferData(gl.ARRAY_BUFFER, ...) will affect
+    // whatever buffer is bound to the `ARRAY_BUFFER` bind point
+    // but so far we only have one buffer. If we had more than one
+    // buffer we'd want to bind that buffer to `ARRAY_BUFFER` first.
+   
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+       x1, y1,
+       x2, y1,
+       x1, y2,
+       x1, y2,
+       x2, y1,
+       x2, y2]), gl.STATIC_DRAW);
   }
 
 module.exports.render = render
