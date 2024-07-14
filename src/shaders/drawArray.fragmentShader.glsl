e4b97997957cbd4208d96f6dc4e9d904c817ae78
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2024-07-14 23:15:25 +0200

    blue

--- src/shaders/drawArray.fragmentShader.glsl
+++ src/shaders/drawArray.fragmentShader.glsl
@@ -6,12 +6,13 @@ precision highp float;
 in vec4 v_position;
 // texture coordinates are in range [0, 1]
 in vec2 v_texcoord;
-
+uniform sampler2D u_texture;
 out vec4 outColor;
 
 void main() {
     //vec2 p1 = (v_position.xy + 1.0) / 2.0;
-    vec2 p = (v_texcoord.xy);
-    float c = sqrt((p.x * p.x) + (p.y * p.y));
-    outColor = vec4(c, c, c, 1);
+    //vec2 p = (v_texcoord.xy);
+    //float c = sqrt((p.x * p.x) + (p.y * p.y));
+    //outColor = vec4(c, c, c, 1);
+    outColor = texture(u_texture, v_texcoord);
 }
