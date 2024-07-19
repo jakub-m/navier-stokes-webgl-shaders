fea991ebdfb5d3ea8c1f813a93e7ced76a990280
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2024-07-20 01:23:11 +0200

    ref

--- src/renderCanvasWithShaders.js
+++ src/renderCanvasWithShaders.js
@@ -38,74 +38,61 @@ function render(args) {
         255, 0,0,255,
     ])
 
-    //renderToTexture(gl, renderToTextureVS, renderToTextureFS, textureA.texture)
     var textureRenderer = new TextureRenderer({gl})
     textureRenderer.renderToTexture(textureA)
 
-    renderTextureToCanvas(gl, drawTextureToScreenVS, drawTextureToScreenFS)
+    var canvasRenderer = new CanvasRenderer({gl})
+    canvasRenderer.render(textureA, textureB)
 }
 
-//function renderToTexture(gl, renderToTextureVS, createTextureFS, targetTexture) {
-//    var program = createProgramFromSources(gl, renderToTextureVS, createTextureFS)
-//    gl.useProgram(program);
-//
-//    var a_position_loc = gl.getAttribLocation(program, "a_position");
-//    var a_texcoord_loc = gl.getAttribLocation(program, "a_texcoord");
-//    var u_input_texture = gl.getUniformLocation(program, "u_input_texture")
-//    validateLocation({a_position_loc, a_texcoord_loc})
-//
-//    var vao = gl.createVertexArray();
-//    gl.bindVertexArray(vao);
-//
-//    initFullSquareTexturePos(gl, a_texcoord_loc)
-//    const vertexCount = initFullSquareVertexPos(gl, a_position_loc)
-//    createOutputAndFrameBufferTexture(gl, targetTexture, 2, 2);
-//
-//    gl.clearColor(0, 0, 0, 0);
-//    gl.clear(gl.COLOR_BUFFER_BIT);
-//
-//    gl.uniform1i(u_input_texture, TEXTURE_ID_A) // Use texture 1 as input texture. We use 0 is for output texture.
-//
-//    gl.drawArrays(gl.TRIANGLES, 0, vertexCount)
-//}
+class CanvasRenderer {
+    constructor({gl}) {
+        validateDefined({gl})
+        resizeCanvasToDisplaySize(gl.canvas);
+        this.gl = gl
+        this.program = createProgramFromSources(gl, drawTextureToScreenVS, drawTextureToScreenFS)
+    }
 
-function renderTextureToCanvas(gl, drawTextureToScreenVS, drawTextureToScreenFS) {
-    validateDefined({gl, drawTextureToScreenVS, drawTextureToScreenFS})
-    resizeCanvasToDisplaySize(gl.canvas);
+    /**
+     * 
+     * @param {Texture} texture_a 
+     * @param {Texture} texture_b 
+     */
+    render(texture_a, texture_b) {
+        var gl = this.gl
+        var program = this.program
+        gl.useProgram(program);
 
-    var program = createProgramFromSources(gl, drawTextureToScreenVS, drawTextureToScreenFS)
-    gl.useProgram(program);
+        var a_position_loc = gl.getAttribLocation(program, "a_position");
+        var a_texcoord_loc = gl.getAttribLocation(program, "a_texcoord");
+        var u_texture_a_loc = gl.getUniformLocation(program, "u_texture_a");
+        var u_texture_b_loc = gl.getUniformLocation(program, "u_texture_b");
+        validateLocation({a_position_loc, a_texcoord_loc})
 
-    var a_position_loc = gl.getAttribLocation(program, "a_position");
-    var a_texcoord_loc = gl.getAttribLocation(program, "a_texcoord");
-    var u_texture_a_loc = gl.getUniformLocation(program, "u_texture_a");
-    var u_texture_b_loc = gl.getUniformLocation(program, "u_texture_b");
-    validateLocation({a_position_loc, a_texcoord_loc})
+        var vao = gl.createVertexArray();
+        gl.bindVertexArray(vao);
 
-    var vao = gl.createVertexArray();
-    gl.bindVertexArray(vao);
+        // TODO Is this initialization needed each time or once?
+        const vertexCount = initFullSquareVertexPos(gl, a_position_loc)
+        initFullSquareTexturePos(gl, a_texcoord_loc)
 
-    const vertexCount = initFullSquareVertexPos(gl, a_position_loc)
-    //initializeTexture(gl, a_texcoord_loc, 0) // TODO here
-    initFullSquareTexturePos(gl, a_texcoord_loc)
-    //initializeTextureValues(gl)
+        gl.uniform1i(u_texture_a_loc, texture_a.texture_id)
+        gl.uniform1i(u_texture_b_loc, texture_b.texture_id) 
+        
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);  // Render to the canvas.
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR); // gl.NEAREST_MIPMAP_LINEAR is default
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
-    gl.uniform1i(u_texture_a_loc, TEXTURE_ID_A)
-    gl.uniform1i(u_texture_b_loc, TEXTURE_ID_B) 
-    
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null);  // Render to the canvas.
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR); // gl.NEAREST_MIPMAP_LINEAR is default
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-
-    // Tell WebGL how to convert from clip space to pixels
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    // Clear the canvas
-    gl.clearColor(0, 0, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    // Tell it to use our program (pair of shaders)
-    gl.drawArrays(gl.TRIANGLES, 0, vertexCount)
+        // Tell WebGL how to convert from clip space to pixels
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+        // Clear the canvas
+        gl.clearColor(0, 0, 0, 0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.drawArrays(gl.TRIANGLES, 0, vertexCount)
+    }
 }
+
 
 function getGlContext(selector) {
     const canvas = document.querySelector(selector);
@@ -138,7 +125,6 @@ class Texture {
     }
 }
 
-
 /**
  * Render program to texture.
  */
@@ -168,16 +154,23 @@ class TextureRenderer {
         // TODO do it only once, if at all.
         initFullSquareTexturePos(gl, a_texcoord_loc)
         const vertexCount = initFullSquareVertexPos(gl, a_position_loc)
-        createOutputAndFrameBufferTexture(gl, target.texture, 2, 2);
+        // TODO do I need to create frame buffer each time, or only once?
+        this._attachFramebuffer(gl, target.texture, target.width, target.height)
 
         gl.clearColor(0, 0, 0, 0);
         gl.clear(gl.COLOR_BUFFER_BIT);
-
         gl.uniform1i(u_input_texture, target.texture_id)
-
         gl.drawArrays(gl.TRIANGLES, 0, vertexCount)
     }
 
+     _attachFramebuffer(gl, texture, width, height) {
+        const fb = gl.createFramebuffer();
+        gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
+        gl.viewport(0, 0, width, height);
+        const attachmentPoint = gl.COLOR_ATTACHMENT0;
+        const level = 0;
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, attachmentPoint, gl.TEXTURE_2D, texture, level);
+    }
 }
 
 
@@ -223,19 +216,6 @@ function initFullSquareVertexPos(gl, a_position_loc) {
     return vertices.length / 2;
 }
 
-//function initializeTexture(gl, a_texcoord_loc, textureNumber) {
-//    gl.activeTexture(gl.TEXTURE0 + textureNumber);
-//    initFullSquareTexturePos(gl, a_texcoord_loc)
-//    initializeTextureValues(gl)
-//}
-
-function createOutputAndFrameBufferTexture(gl, targetTexture, width, height) {
-    validateDefined({gl, targetTexture, width, height})
-    // console.log("createOutputTexture: outputTexture")
-    // initializeTexture(gl, targetTexture, width, height)
-    attachFramebuffer(gl, targetTexture, width, height)
-}
-
 /**
  * data parameter is optional, if null then the data in the texture is undefined.
  */
@@ -262,16 +242,6 @@ function initializeTexture(gl, texture, width, height, data) {
 
 }
 
-function attachFramebuffer(gl, texture, width, height) {
-    // Create and bind the framebuffer
-    const fb = gl.createFramebuffer();
-    gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
-    gl.viewport(0, 0, width, height);
-    // attach the texture as the first color attachment
-    const attachmentPoint = gl.COLOR_ATTACHMENT0;
-    const level = 0;
-    gl.framebufferTexture2D(gl.FRAMEBUFFER, attachmentPoint, gl.TEXTURE_2D, texture, level);
-}
 
 
 function initFullSquareTexturePos(gl, a_texcoord_loc) {
@@ -302,24 +272,6 @@ function initFullSquareTexturePos(gl, a_texcoord_loc) {
         offset: 0,
     });
 }
-
-// function initializeTextureValues(gl) {
-//     validateDefined({gl})
-//     var texture = gl.renderToTexture();
-//     gl.bindTexture(gl.TEXTURE_2D, texture);
-//     // TODO check larger texture
-//     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 2, 2, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array(
-//         [
-//             0, 0, 255, 255, // bottom-left
-//             0, 0, 255, 255, // bottom-right
-//             255, 0, 0, 255, // red top-left
-//             0, 255, 0, 255, // green top-right
-//         ]
-//     ));
-//     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR); // gl.NEAREST_MIPMAP_LINEAR is default
-//     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-//     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-// }
 
 function gl_vertexAttribPointer(args) {
     const {gl, index, size, type, normalize, stride, offset} = args
@@ -414,5 +366,4 @@ function createProgram(gl, vertexShader, fragmentShader) {
 }
 
 
-//module.exports = {render}
 export {render}
