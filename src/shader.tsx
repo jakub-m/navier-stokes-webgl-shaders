e9abd28b6349ca1be3043df080d064000ae3dc49
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2024-07-21 01:57:56 +0200

    play button

--- src/shader.tsx
+++ src/shader.tsx
@@ -1,23 +1,59 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 
 //const renderCanvasWithShaders = require('./renderCanvasWithShaders')
 import {render} from './renderCanvasWithShaders'
+import { request } from "http";
 
 
 export interface ShaderProps {
+  run: boolean
   custom: {[key: string]: any}
 }
 
 export const Shader = ({custom}: ShaderProps) => {
+  const [run, setRun] = useState(false);
+  const requestRef = useRef<number>()
+  const prevTimeRef = useRef(0)
+
+  const animate = useCallback((time: number) => {
+    // `time` is in ms
+    var deltaMs = 0;
+    if (prevTimeRef.current !== 0) {
+      deltaMs = time - prevTimeRef.current
+    }
+    prevTimeRef.current = time
+    console.log(1000/deltaMs + " fps");
+    render({custom})
+    requestRef.current = requestAnimationFrame(animate);
+  }, [custom])
+
   useEffect(() => {
-    render({
-        //drawTextureToScreenVS: drawTextureVS,
-        //drawTextureToScreenFS: drawTextureFS,
-        //createTextureVS,
-        //createTextureFS,
-        custom,
-    })
-  }, [custom]);
- return <canvas id="c"></canvas>
+    if (run) {
+      requestRef.current = requestAnimationFrame(animate);
+    } else {
+      render({custom})
+    }
+    return () => {
+      if (requestRef.current === undefined) {
+        return
+      }
+      cancelAnimationFrame(requestRef.current)
+    };
+  }, [animate, run, custom]);
+
+  //useEffect(() => {
+  //  render({
+  //      custom,
+  //  })
+  //}, [custom]);
+ const pausePlayButton = (run) ? "stop" : "play";
+ const handleClickPlay = () => {
+  setRun(r => !r)
+ }
+ return (
+  <>
+    <canvas id="c"></canvas>
+    <div onClick={handleClickPlay}>{pausePlayButton}</div>
+  </>)
 };
